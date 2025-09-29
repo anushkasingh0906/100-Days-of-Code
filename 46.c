56f7012534a984aa79eb90999065a01af80c5647
anushkasingh0906 <anushkasingh14906@gmail.com>
2025-09-30 02:57:22 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,46 @@
+Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the numbers of rows \n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+OUTPUT
+Enter the numbers of rows 
+8
+********
+********
+********
+********
+********
+********
+********
+********
