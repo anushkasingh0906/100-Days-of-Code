bbb035f6fc9b641ebed20d0752c7507746b4cbf6
anushkasingh0906 <anushkasingh14906@gmail.com>
2025-10-01 02:22:16 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,80 @@
+Q54: Write a program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+
+*/
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter tha number of rows \n");
+    scanf("%d",&n);
+
+   for(int i=1; i<=n ; i++)                //  for loop for the rows 
+    {
+        for(int j=1; j<=n-i; j++)           // loop  for spaces from left to right
+        {
+            printf(" ");
+        }
+        for(int j=1; j<=i; j++)           
+        {
+            printf("* ");
+        }
+        for(int j=1; j<=n-i; j++)          // loop to print the spaces  
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+    for(int i=n-1;i>=1;i--)             // print the reverse of the first loop 
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+OUTPUT
+Enter tha number of rows 
+9
+        *         
+       * *        
+      * * *       
+     * * * *      
+    * * * * *     
+   * * * * * *    
+  * * * * * * *   
+ * * * * * * * *  
+* * * * * * * * * 
+ * * * * * * * *  
+  * * * * * * *   
+   * * * * * *    
+    * * * * *     
+     * * * *      
+      * * *       
+       * *        
+        *    
