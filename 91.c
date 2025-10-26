91fe1e3c7d436e41a1e29825a5a5e1d01721eabe
anushkasingh0906 <anushkasingh14906@gmail.com>
2025-10-26 15:03:50 +0530

    Add program to remove vowels from a string
    This program removes all vowels from the input string and prints the result.

---
+++ 91.c
@@ -0,0 +1,38 @@
+Q91: Remove all vowels from a string.
+
+/*
+Sample Test Cases:
+Input 1:
+education
+Output 1:
+dctn
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], result[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    for (i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+        if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' &&
+            ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U') {
+            result[j++] = ch;
+        }
+    }
+    result[j] = '\0';
+
+    printf("String without vowels: %s", result);
+    return 0;
+}
+
+OUTPUT:
+Enter a string: hello how are you @123
+String without vowels: hll hw r y @123
