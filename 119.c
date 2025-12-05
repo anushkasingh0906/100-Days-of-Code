8989996f73e836c8e38445877ebb176dffc42635
anushkasingh0906 <anushkasingh14906@gmail.com>
2025-12-06 00:21:56 +0530

    Add function to find first repeated number
    Implement a program to find the first repeated number in an array.

---
+++ 119.c
@@ -0,0 +1,49 @@
+Q119: �Write a program to take an integer array as input. Only one element will be repeated. Print the repeated element. Try to find the result in one single iteration.
+
+/*
+Input 1:
+nums1 = [1,3,3,4]
+Output 1:
+3
+Input 2:
+nums1 = [1,2,2]
+Output 2:
+2
+Input 3:
+nums1 = [0,4,1,1,5]
+Output 3:
+1
+*/
+
+input:
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Maximum value in array won't exceed n (based on given pattern)
+    int visited[n+1];
+    for (int i = 0; i <= n; i++)
+        visited[i] = 0;
+
+    int repeated = -1;
+
+    // One single iteration
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (visited[arr[i]] == 1) {
+            repeated = arr[i];
+        }
+
+        visited[arr[i]] = 1;
+    }
+
+    printf("%d", repeated);
+
+    return 0;
+}
